e7271232366dd99ed99305e383acbcb0c59cc77e
leo-usa <56208994+leo-usa@users.noreply.github.com>
2025-05-08 16:22:35 -0700

    Updated Stripe product IDs to my own to be consistent with the backend

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -33,35 +33,35 @@ interface Config {
 // Production tier IDs
 const PROD_TIERS: SubscriptionTiers = {
   FREE: {
-    priceId: 'price_1RILb4G6l1KZGqIrK4QLrx9i',
+    priceId: 'price_1RMBu0P2cIDuyWfbQGxAsOZ7',
     name: 'Free',
   },
   TIER_2_20: {
-    priceId: 'price_1RILb4G6l1KZGqIrhomjgDnO',
+    priceId: 'price_1RMBKOP2cIDuyWfbH6DrdJoH',
     name: '2h/$20',
   },
   TIER_6_50: {
-    priceId: 'price_1RILb4G6l1KZGqIr5q0sybWn',
+    priceId: 'price_1RMBRfP2cIDuyWfbmh1dYuQg',
     name: '6h/$50',
   },
   TIER_12_100: {
-    priceId: 'price_1RILb4G6l1KZGqIr5Y20ZLHm',
+    priceId: 'price_1RMBRfP2cIDuyWfbMUDqfrgy',
     name: '12h/$100',
   },
   TIER_25_200: {
-    priceId: 'price_1RILb4G6l1KZGqIrGAD8rNjb',
+    priceId: 'price_1RMBRfP2cIDuyWfby99jVvXT',
     name: '25h/$200',
   },
   TIER_50_400: {
-    priceId: 'price_1RILb4G6l1KZGqIruNBUMTF1',
+    priceId: 'price_1RMBRfP2cIDuyWfbHMBX01JN',
     name: '50h/$400',
   },
   TIER_125_800: {
-    priceId: 'price_1RILb3G6l1KZGqIrbJA766tN',
+    priceId: 'price_1RMBRfP2cIDuyWfbVPuRpDkq',
     name: '125h/$800',
   },
   TIER_200_1000: {
-    priceId: 'price_1RILb3G6l1KZGqIrmauYPOiN',
+    priceId: 'price_1RMBRfP2cIDuyWfbJZh9gNHu',
     name: '200h/$1000',
   }
 } as const;
